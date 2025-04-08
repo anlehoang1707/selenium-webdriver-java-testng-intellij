f11f025670238352e266a8774fac6ce0601365b3
leanprojectmanager <an.lehoang1707@gmail.com>
2025-04-08 22:18:45 +0700

    user interactions selenium practice

---
+++ selenium/webdriver/Topic_17_Actions_I.java
@@ -0,0 +1,41 @@
+ package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.Color;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+ public class Topic_17_Actions_I {
+     //Khai báo Driver và Actions.
+     WebDriver driver;
+     Actions action;
+
+     @BeforeClass
+     public void initialBrowser() {
+         driver = new FirefoxDriver();
+         driver.get("https:/demo.nopcommerce.com/register?returnUrl=%2F");
+         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+         action = new Actions(driver);
+
+     }
+
+     @Test
+     public void TC_01_() {
+
+
+     }
+
+     @AfterClass
+     public void closeBroswer() {
+         driver.quit();
+     }
+
+ }
