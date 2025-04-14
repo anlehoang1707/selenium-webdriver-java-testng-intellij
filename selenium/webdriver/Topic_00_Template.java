9033cbcf62e4a437a85aa9eb3def0c6e89b1d71f
leanprojectmanager <an.lehoang1707@gmail.com>
2025-04-15 00:30:18 +0700

    Update Template

---
+++ selenium/webdriver/Topic_00_Template.java
@@ -0,0 +1,31 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Topic_00_Template {
+    WebDriver driver;
+
+    @BeforeClass
+    public void initialBrowser() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @Test
+    public void TC_01_() {
+        Assert.assertTrue(false);
+    }
+
+    @AfterClass
+    public void cleanBrowser() {
+        driver.quit();
+    }
+}
